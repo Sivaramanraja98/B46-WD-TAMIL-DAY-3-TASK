852a598120ab282d5e12f3e91be83d1fef5d23b4
Sivaramanraja98 <127581183+Sivaramanraja98@users.noreply.github.com>
2023-03-18 15:01:30 +0530

    Create jsonforeach.js

---
+++ jsonforeach.js
@@ -0,0 +1,10 @@
+var chat = [
+   {"id":1,"message":"hello"}, 
+   {"id":2,"message":"hai"},
+   {"id":3,"message":"hey"}
+];
+   
+    
+    chat.forEach(function(k,v) {
+      console.log(k.id+" : "+k.message);
+    });
