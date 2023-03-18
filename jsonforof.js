65f91b5c3bf1951251d64d010677a998edc87c52
Sivaramanraja98 <127581183+Sivaramanraja98@users.noreply.github.com>
2023-03-18 14:51:23 +0530

    Create jsonforof.js

---
+++ jsonforof.js
@@ -0,0 +1,12 @@
+let chat =
+[
+   {"id":1,"message":"hello"}, 
+   {"id":2,"message":"hai"},
+   {"id":3,"message":"hey"}
+];
+
+
+for(let value of chat)
+{
+    console.log(value.id+" : "+value.message);
+}
